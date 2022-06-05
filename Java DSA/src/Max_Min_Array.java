ab0816bea42ff78f958d168e73ae65667e1d299a
DELL <DELL@DESKTOP-58V60OV>
2022-06-05 16:33:35 +0530

    min_max

--- Java DSA/src/Max_Min_Array.java
+++ Java DSA/src/Max_Min_Array.java
@@ -2,8 +2,18 @@
 public class Max_Min_Array {
 
 	public static void main(String[] args) {
-		int arr[]= {45,29,14,146,47};
-		
+		int arr[]= {45,29,14,0,146,1000,47};
+		int max=arr[0],min=arr[0];
+		for(int i=1;i<arr.length-1;i++) {
+			if(arr[i]>max) {
+				max=arr[i];
+			}
+			if(arr[i]<min) {
+				min=arr[i];
+			}
+		}
+		System.out.println("Maximum element is:"+max);
+		System.out.println("Minimum element is:"+min);
 	}
 
 }
